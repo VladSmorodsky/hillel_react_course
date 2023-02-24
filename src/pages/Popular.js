b357730fffb464ad3a609f70b7d02e45e7209770
VSmorodskyi <smorodsky.odesa@gmail.com>
2023-02-25 00:18:20 +0200

    [HRC-5] Create Search functionality

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,7 +1,119 @@
-import {Tabs} from "../components/Tabs/Tabs";
+import {useEffect, useState} from "react";
+import {fetchPopularRepositories, fetchSearchResult} from "../api/githubApi";
+import {LanguageList} from "../components/LanguageList";
+import {RepositoryList} from "../components/RepositoryList";
+import {useSearchParams} from "react-router-dom";
 
-export const Popular = () =>
-  <div className="popular-container">
-    <h2>Popular</h2>
-    <Tabs />
-  </div>
+const languages = ["All", 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+const allowSymbols = /[A-Za-z]+/g;
+
+export const Popular = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  let [searchResultRepo, setSearchResultRepo] = useState([]);
+  let [searchRepoQuery, setSearchRepoQuery] = useState('');
+  let [isSearching, setIsSearching] = useState(false);
+  let [validationError, setValidationError] = useState(null);
+
+  let [repositories, setRepositories] = useState(null);
+  let [error, setError] = useState(null);
+  let [loading, setLoading] = useState(false)
+
+  let selectedLanguage =  searchParams.get('lang');
+
+  useEffect(() => {
+    setLoading(true);
+    if (!selectedLanguage || !languages.includes(selectedLanguage)) {
+      setSearchParams({lang: 'All'});
+    }
+    try {
+      const getRepoData = async () => {
+        const repositories = await fetchPopularRepositories(selectedLanguage);
+        setLoading(false);
+        setRepositories(repositories);
+      };
+
+      getRepoData();
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [selectedLanguage]);
+
+  useEffect(() => {
+    setIsSearching(true);
+    setValidationError(null);
+
+    if (!allowSymbols.test(searchRepoQuery) && searchRepoQuery !== "") {
+      setValidationError('Use only latin characters');
+      return;
+    }
+
+    if (searchRepoQuery === "") {
+      setIsSearching(false);
+      return;
+    }
+
+    setLoading(true);
+
+    const searchRepo = setTimeout(() => {
+      fetchSearchResult(searchRepoQuery)
+        .then(resultItems => {
+          setSearchResultRepo(resultItems);
+        }).catch((error) => setError(error))
+        .finally(() => {
+          setLoading(false);
+        });
+    }, 2000);
+
+    return () => {
+      clearTimeout(searchRepo);
+    }
+  }, [searchRepoQuery]);
+
+  const clearInput = (event) => {
+    if(event.key === "Escape") {
+      setSearchRepoQuery("");
+    }
+  }
+
+  const showContent = () => {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (error) {
+      return <p>{error}</p>
+    }
+
+    if (searchRepoQuery.length && isSearching && validationError === null) {
+      return <RepositoryList repositories={searchResultRepo} />;
+    }
+
+    return repositories?.length ? <RepositoryList repositories={repositories} /> : null;
+  }
+
+  return (
+    <>
+      <div className="container d-flex align-items-center justify-content-between">
+        <LanguageList languages={languages}
+                      selectedLanguage={selectedLanguage}
+                      setSearchParams={setSearchParams}
+        />
+        <div className="search-container">
+          <input type="text"
+                 className={validationError ? 'validation-error' : ''}
+                 value={searchRepoQuery}
+                 onChange={(event) => setSearchRepoQuery(event.target.value)}
+                 onKeyDown={(event) => clearInput(event)}
+                 placeholder="Search repo..."
+          />
+          {validationError ? <span className="validation-error-message">{validationError}</span> : null}
+        </div>
+      </div>
+      {showContent()}
+    </>
+  );
+}
+
